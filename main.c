7d603a3e29b6448873bf6a02417e571a8da9dd90
Shawn Trznadel <Strznadel1@gmail.com>
2021-02-04 22:27:19 -0500

    Update main.c
    Updated

--- main.c
+++ main.c
@@ -1,34 +1,51 @@
 #include <stdio.h> 
 #include <time.h>
-#include <unistd.h>
 
-void Append_To_End_Of_File(FILE *origional, FILE *second_file, FILE *new_file);
+void Append_To_End_Of_Origional_File(FILE *Origional, FILE *second_file);
+void Write_To_Output_File(FILE *origional, FILE *second_file, FILE *new_file);
 
 int main(int argc, char* argv[]){
     clock_t begin = clock();
     
-    // opening the origional file. 
-    char* origional_file = argv[1];
-    char* second_file_name = argv[2];
-    char* new_file_name = argv[3];
-    if(access(origional_file, F_OK) == 0){
-        FILE *fp = fopen(origional_file, "r");
+    if(argc == 4){
+        // opening the files.
+        char* origional_file_name = argv[1];
+        char* second_file_name = argv[2];
+        char* new_file_name = argv[3];
+        FILE *origional_file = fopen(origional_file_name, "r");
         FILE *second_file = fopen(second_file_name, "r");
         FILE *new_file = fopen(new_file_name, "w");
 
         //File could not open.
-        if(second_file == NULL){
+        if(origional_file == NULL){
+            printf("Error opening %s", origional_file_name);
+        }
+        else if(second_file == NULL){
+             printf("Error opening %s", second_file_name);
+        }
+
+        //Files were opened and now writing to the new file.
+        Write_To_Output_File(origional_file, second_file, new_file);
+    }
+    else if(argc == 3){
+        char* origional_file_name = argv[1];
+        char* second_file_name = argv[2];
+
+        // Opening the files.
+        FILE *origional_file = fopen(origional_file_name, "a");
+        FILE *second_file = fopen(second_file_name, "r");
+
+        if(origional_file == NULL || second_file == NULL){
+            printf("Error opening %s", origional_file_name);
+        }
+        else if(second_file == NULL){
             printf("Error opening %s", second_file_name);
         }
-        
-        //File was opened, and now appending. 
-        Append_To_End_Of_File(fp, second_file, new_file);
 
+        //Files were oppened and now appening the second file to the origional file. 
+        Append_To_End_Of_Origional_File(origional_file, second_file);
     }
-    else{
-        printf("Origional file does not exist.");
-    }
-
+    
     // End of program prints time.
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
@@ -36,7 +53,19 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-void Append_To_End_Of_File(FILE *origional, FILE *second_file, FILE *new_file){
+void Append_To_End_Of_Origional_File(FILE *origional_file, FILE *second_file){
+    fprintf(origional_file, "%c", '\n');
+    while(1){
+        char character = fgetc(second_file);
+        if(character == EOF){
+            break;
+        }
+        fprintf(origional_file, "%c", character);
+    }
+}
+
+
+void Write_To_Output_File(FILE *origional, FILE *second_file, FILE *new_file){
     while(1){
         char character = fgetc(origional);
         if(character == EOF){
